b7f8d6468b46fab8c8b5d3ba7658624e59ea100a
Exile404 <tahsinul.haque.dhrubo@gmail.com>
2023-12-31 00:20:01 +0600

    Port Error Removed

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def handle_message(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True,PORT= 8000)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
